2fc569dc75dbe723ea7be2a30735901653bc5a72
Kydon <130697271+kydon-ai@users.noreply.github.com>
2026-01-30 11:45:34 +0800

    feat:初始化仓库

---
+++ main.py
@@ -0,0 +1,5 @@
+
+
+if __name__ == '__main__':
+    # auto_commit()
+    print('auto commit done')
---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 获取当前日期时间的不同组成部分
+current_year=$(date +%Y)
+current_month=$(date +%m)
+current_day=$(date +%d)
+current_time=$(date +%H%M%S)
+
+# 构建完整的目录路径
+target_dir="./data/${current_year}/${current_month}/${current_day}/${current_time}"
+
+# 创建多级目录（如果不存在）
+mkdir -p "$target_dir"
+
+# 创建空的crops.txt文件
+touch "${target_dir}/crops.txt"
+
+# git 部分
+git add .
+git commit -m "auto commit"
+git push origin main
